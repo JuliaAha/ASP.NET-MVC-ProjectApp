ad1191b657900e692a138a6ea27052ee642b0a78
juliaAha <julg@kth.se>
2024-10-18 17:27:18 +0200

    corrected Areas

--- ProjectApp/Areas/Identity/Data/AppIdentityDbContext.cs
+++ ProjectApp/Areas/Identity/Data/AppIdentityDbContext.cs
@@ -1,12 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProjectApp.Areas.Identity.Data;
 
-namespace ProjectApp.Areas.Identity.Data;
+namespace ProjectApp.Data;
 
-// Add profile data for application users by adding properties to the AppIdentityUser class
-public class AppIdentityUser : IdentityUser
+public class AppIdentityDbContext : IdentityDbContext<AppIdentityUser>
 {
+    public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
 }
--- ProjectApp/Areas/Identity/Data/AppIdentityUser.cs
+++ ProjectApp/Areas/Identity/Data/AppIdentityUser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
-namespace ProjectApp.Areas.Identity.Data.ProjectApp.Data;
+namespace ProjectApp.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the AppIdentity class
 public class AppIdentityUser : IdentityUser
